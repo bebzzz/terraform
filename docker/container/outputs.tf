f8daa1086fa263f2f4fed7eeba7c76e868aa4bf7
root <root@abebych6.mylabserver.com>
2018-05-31 13:03:40 +0000

    first commit

---
+++ docker/container/outputs.tf
@@ -0,0 +1,8 @@
+# Output the IP address and name of the container
+output "ip" {
+	value = "${docker_container.container_id.ip_address}"
+}
+
+output "container_name" {
+        value = "${docker_container.container_id.name}"
+}
